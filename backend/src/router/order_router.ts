2e5ca73a91df5e9341145b0f7f83bb41beda661b
Nam Anh Tran <steve200994@gmail.com>
2025-12-08 10:42:10 +1100

    Add order cancellation routes to order router

--- backend/src/router/order_router.ts
+++ backend/src/router/order_router.ts
@@ -4,12 +4,26 @@ const router = express.Router();
 import {
   create_order,
   list_order,
-  create_checkout
+  create_checkout,
+  cancel_order_system,
+  cancel_order_customer
 } from "../controller/order_controller";
 
 import { require_auth } from "../middleware/require_auth";
-router.post("/create", require_auth, create_order);
-router.get("/list", require_auth, list_order);
-router.post("/checkout", require_auth, create_checkout);
+router.post("/order/create", require_auth, create_order);
+router.get("/order/list", require_auth, list_order);
+router.post("/order/checkout", require_auth, create_checkout);
+
+//consider PATCH
+router.put(
+  "/order/cancel/:order_id",
+  require_auth,
+  cancel_order_customer
+);
+router.put(
+  "/order/system-cancel/:order_id",
+  require_auth,
+  cancel_order_system
+);
 
 export default router;
