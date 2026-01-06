4bde165498c246b0685a0dec8adc62f6317c503f
Nam Anh Tran <steve200994@gmail.com>
2026-01-06 16:38:02 +1100

    Add founder images

--- frontend/hired/src/app/about-us/about-us.ts
+++ frontend/hired/src/app/about-us/about-us.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   imports: [],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
 export class AboutUs {
 
